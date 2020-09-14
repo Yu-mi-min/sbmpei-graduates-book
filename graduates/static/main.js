19dbe11c40b4679189b3d2df2f5bb664d6b8f90f
Mikhail Pashkov <workpashkovmikhail@gmail.com>
2020-09-14 16:02:04 +0300

    refactor main.js

--- graduates/static/main.js
+++ graduates/static/main.js
@@ -1,29 +1,38 @@
 let book = $('#book');
-
-book.turn({
-    height: 1000,
-    shadow: true,
-})
-
-book.bind('turning', (event, page, view) => {
-    let current = $('#current-view');
-    if (view[0] === 0 || view[1] === 0)
-        current.val(view[0] + view[1]);
-    else
-        current.val(`${view[0]} \u2013 ${view[1]}`);
-});
+let ddSpec = $('#dd-spec');
+let ddMast = $('#dd-mast');
+let ddLinks = $('.to-page');
+let modalInfo = $('#modal-info');
+let currentView = $('#current-view');
 
 prevPage = () => book.turn('previous');
 nextPage = () => book.turn('next');
 toPage = (page) => book.turn('page', page)
 
-let ddSpec = $('#dd-spec')
-let ddMast = $('#dd-mast')
-
 hideDDContents = () => {
     ddSpec.removeClass('is-active');
     ddMast.removeClass('is-active');
 }
+toggleDropdown = (dd) => dd.toggleClass('is-active');
+
+modalInfoOpen = () => modalInfo.addClass('is-active');
+modalInfoClose = () => modalInfo.removeClass('is-active');
+
+hideLoader = () => setTimeout(() => $('#loader').removeClass('is-active'), 555);
+
+book.turn({
+    height: 1000,
+    shadow: true,
+});
+
+currentView.val(1)
+
+book.bind('turning', (event, page, view) => {
+    if (view[0] === 0 || view[1] === 0)
+        currentView.val(view[0] + view[1]);
+    else
+        currentView.val(`${view[0]} \u2013 ${view[1]}`);
+});
 
 $(document).keydown(e => {
     switch (e.code) {
@@ -39,27 +48,17 @@ $(document).keydown(e => {
     }
 })
 
-clickHandler = (dd) => dd.toggleClass('is-active');
-ddSpec.click(() => clickHandler(ddSpec))
-ddMast.click(() => clickHandler(ddMast))
+ddSpec.click(() => toggleDropdown(ddSpec))
+ddMast.click(() => toggleDropdown(ddMast))
 
-$('.to-page').click(e => { // <a> in dropdown
-    e.stopPropagation()
+ddLinks.click(e => {
+    e.stopPropagation();
     let page = e.target.getAttribute('to-page');
     toPage(page);
     hideDDContents();
 });
 
-let modalInfo = $('#modal-info');
-
-modalInfoOpen = () => {
-    modalInfo.addClass('is-active');
-    $('#dh').focus()
-
-}
-modalInfoClose = () => modalInfo.removeClass('is-active')
-
-modalInfo.children('.modal-background').click(() =>modalInfoClose());
+modalInfo.children('.modal-background').click(() => modalInfoClose());
 modalInfo.children('.modal-close').click(() => modalInfoClose());
 
-setTimeout(() => $('#loader').removeClass('is-active'), 555);
+hideLoader();
